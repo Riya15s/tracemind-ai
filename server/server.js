ebfa3c85763b94bc3b2a1d61e7deb3ca80a183d4
riya15s <riyajagriti412@gmail.com>
2026-07-06 00:31:41 +0530

    Prepare frontend for deployment

--- server/server.js
+++ server/server.js
@@ -2,16 +2,12 @@ const dotenv = require("dotenv");
 const app = require("./app");
 const connectDB = require("./config/db");
 
-// Load environment variables
-
 dotenv.config();
-console.log(process.env.MONGODB_URI);
 
-// Then connect to MongoDB
 connectDB();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(` Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
